81921b5c0880849109337a36a1616aa500e75a8a
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-03-31 13:48:43 -0500

    deploying

--- src/datatable/index.jsx
+++ src/datatable/index.jsx
@@ -3,14 +3,24 @@ import React from 'react';
 export default function Datatable({data})
 {
     return(
-        <div className="personDiv" style={{textAlign:'center'}}>
+        <div className="personDiv" style={{display:'inline'}}>
             {
                 data.map((person, index)=>{
                     return(
+
                         <div key={index}>
-                            <h3>{person.name.first} {person.name.last}</h3>
+                        <div className = "row">
+                            <div className="three columns">
+                            <h3>{person.name.first} {person.name.last}</h3></div>
+
+                            <div className="six columns">
                             <p>gender: {person.gender}</p>
+                            </div>
+
+                            <div className="three columns">
                             <p>email: {person.email}</p>
+                            </div>
+                        </div>
                         </div>
                     )
                 })
